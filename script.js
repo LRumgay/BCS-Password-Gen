29673519a81524c4a39aa179cc74eab4cf7e8eb4
Liam Rumgay <Liamrumgay2@gmail.com>
2021-10-05 19:37:52 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var characterAmountRange = document.getElementById("characterAmountRange");
-var characterAmountNumber = document.getElementById("characterAmountNumber");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);{
-}
